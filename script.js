44b25ce08399b28054ec61e0ace3668b01c4107b
Emilio Resende <emilio.murta@msitbrasil.com>
2023-06-22 08:30:07 -0300

    prisma init

--- script.js
+++ script.js
@@ -10,15 +10,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+const runtime_1 = require("@prisma/client/runtime");
+const prisma = new client_1.PrismaClient({
+    log: ["query", "info", "warn", "error"],
+});
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        const userDelete = yield prisma.user.delete({
-            where: {
-                email: "alice@prisma.io",
-            },
-        });
-        console.log(userDelete);
+        try {
+            const deletedPosts = yield prisma.post.deleteMany({
+                where: {
+                    id: {
+                        gt: 0,
+                    },
+                },
+            });
+            console.log(deletedPosts);
+            const deletedUsers = yield prisma.user.deleteMany({
+                where: {
+                    id: {
+                        gt: 0,
+                    },
+                },
+            });
+            console.log(deletedUsers);
+        }
+        catch (e) {
+            if (e instanceof runtime_1.PrismaClientKnownRequestError) {
+                console.log(e.code);
+                console.log(e.message);
+            }
+        }
         const user = yield prisma.user.create({
             data: {
                 name: "Alice",
@@ -28,6 +49,75 @@ function main() {
         console.log(user);
         const users = yield prisma.user.findMany();
         console.log(users);
+        const userWithPost = yield prisma.user.create({
+            data: {
+                name: "Bob",
+                email: "bob@prisma.io",
+                posts: {
+                    create: {
+                        title: "Timber Post Title",
+                        content: "Timber Post"
+                    },
+                },
+            },
+        });
+        console.log(userWithPost);
+        const usersWithPosts = yield prisma.user.findMany({
+            include: {
+                posts: true,
+            },
+        });
+        console.dir(usersWithPosts, { depth: null });
+        console.log("======================");
+        console.log("queries...");
+        console.log("======================");
+        console.log("left join...");
+        const usrAndPostLeft = yield prisma.user.findMany({
+            include: {
+                posts: true,
+            },
+        });
+        console.log(usrAndPostLeft);
+        console.log("inner join...");
+        const usrAndPostInner = yield prisma.user.findMany({
+            include: {
+                posts: true,
+            },
+            where: {
+                posts: {
+                    some: {
+                        NOT: {
+                            id: 0,
+                        },
+                    },
+                },
+            },
+        });
+        console.log(usrAndPostInner);
+        console.log("repository...");
+        class PostRepository {
+            findByTitleAndText(title, content) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    return yield prisma.post.findMany({
+                        where: {
+                            title: title,
+                            content: {
+                                contains: content,
+                            },
+                        },
+                    });
+                });
+            }
+        }
+        const timberPost = yield new PostRepository().findByTitleAndText("Timber Post Title", "Timber");
+        console.log(timberPost);
+        console.log("native queries...");
+        const r = yield prisma.$queryRaw(client_1.Prisma.sql `select * from user`);
+        console.log(r);
+        const r2 = yield prisma.$queryRaw(client_1.Prisma.sql `select * from user`);
+        r2.forEach((us) => {
+            console.log(us);
+        });
     });
 }
 main()
--- script.ts
+++ script.ts
@@ -1,14 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, User } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
 
 async function main() {
-  const userDelete = await prisma.user.delete({
-    where: {
-      email: "alice@prisma.io",
-    },
-  });
-  console.log(userDelete);
+  try {
+    const deletedPosts = await prisma.post.deleteMany({
+      where: {
+        id: {
+          gt: 0,
+        },
+      },
+    });
+    console.log(deletedPosts);
+
+    const deletedUsers = await prisma.user.deleteMany({
+      where: {
+        id: {
+          gt: 0,
+        },
+      },
+    });
+    console.log(deletedUsers);
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      console.log(e.code);
+      console.log(e.message);
+    }
+  }
+
   const user = await prisma.user.create({
     data: {
       name: "Alice",
@@ -16,8 +38,85 @@ async function main() {
     },
   });
   console.log(user);
+
   const users = await prisma.user.findMany();
   console.log(users);
+
+  const userWithPost = await prisma.user.create({
+    data: {
+      name: "Bob",
+      email: "bob@prisma.io",
+      posts: {
+        create: {
+          title: "Timber Post Title",
+          content: "Timber Post"
+        },
+      },
+    },
+  });
+  console.log(userWithPost);
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(usersWithPosts, { depth: null });
+
+  console.log("======================");
+  console.log("queries...");
+  console.log("======================");
+  console.log("left join...");
+  const usrAndPostLeft = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.log(usrAndPostLeft);
+
+  console.log("inner join...");
+  const usrAndPostInner = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+    where: {
+      posts: {
+        some: {
+          NOT: {
+            id: 0,
+          },
+        },
+      },
+    },
+  });
+  console.log(usrAndPostInner);
+
+  console.log("repository...");
+  class PostRepository {
+    public async findByTitleAndText(title: string, content: string) {
+      return await prisma.post.findMany({
+        where: {
+          title: title,
+          content: {
+            contains: content,
+          },
+        },
+      });
+    }
+  }
+  const timberPost = await new PostRepository().findByTitleAndText(
+    "Timber Post Title",
+    "Timber"
+  );
+  console.log(timberPost);
+
+  console.log("native queries...");
+  const r = await prisma.$queryRaw(Prisma.sql`select * from user`);
+  console.log(r);
+
+  const r2 = await prisma.$queryRaw<User[]>(Prisma.sql`select * from user`);
+  r2.forEach((us) => {
+    console.log(us);
+  });
 }
 
 main()
